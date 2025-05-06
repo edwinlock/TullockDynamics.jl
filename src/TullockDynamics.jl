66b6d81bd37cac5aeb36b80c533ad6ebacf4189a
Edwin Lock <mail@edwinlock.com>
2025-05-06 12:38:19 +0100

    export max_effort function

--- src/TullockDynamics.jl
+++ src/TullockDynamics.jl
@@ -15,7 +15,7 @@ include("utils.jl")
 
 # Exports from agents.jl
 export Agent, MLEAgent, DetMLEAgent, DumbAgent, BayesianAgent
-export utility, best_response, nash_gap
+export utility, best_response, nash_gap, max_effort
 # Exports from contests.jl
 export TullockContest
 export nash_gap, numrounds
